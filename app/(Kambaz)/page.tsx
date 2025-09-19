06f4b4b96765adb4dcc03dc6ab8caf598f17d645
miguelit07 <148008820+miguelit07@users.noreply.github.com>
2025-09-18 21:14:20 -0400

    landing pages

---
+++ app/(Kambaz)/page.tsx
@@ -0,0 +1,11 @@
+export default function Kambaz() {
+  return (
+    <html>
+      <body>
+        <div id="wd-kambaz">
+          <h1>Kambaz</h1>
+        </div>
+      </body>
+    </html>
+  );
+}
