b540b009a2fc82b6574b3e08e581100fe7c0a98a
miguelit07 <148008820+miguelit07@users.noreply.github.com>
2025-09-19 00:38:14 -0400

    assignment editor

--- app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
@@ -2,12 +2,15 @@ export default function AssignmentEditor() {
   return (
     <div id="wd-assignments-editor">
       <label htmlFor="wd-name">Assignment Name</label>
-      <input id="wd-name" defaultValue="A1 - ENV + HTML" /><br /><br />
+      <input id="wd-name" defaultValue="A1 - ENV + HTML" />
+      <br />
+      <br />
       <textarea id="wd-description">
         The assignment is available online Submit a link to the landing page of
       </textarea>
       <br />
       <table>
+        <br />
         <tr>
           <td align="right" valign="top">
             <label htmlFor="wd-points">Points</label>
@@ -16,7 +19,141 @@ export default function AssignmentEditor() {
             <input id="wd-points" defaultValue={100} />
           </td>
         </tr>
-        {/* Complete on your own */}
+        <br />
+        <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-group">Assignment Group</label>
+          </td>
+          <td>
+            <select id="wd-group" name="wd-group">
+              <option value="">Select a group</option>
+              <option value="group1">QUIZES</option>
+              <option value="group2">LABS</option>
+              <option value="group3">EXAMS</option>
+            </select>
+          </td>
+        </tr>
+        <br />
+        <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-display-grade-as">Display Grade as </label>
+          </td>
+          <td>
+            <select id="wd-group" name="wd-group">
+              <option value="">Select a group</option>
+              <option value="group1">PERCENTAGE</option>
+              <option value="group2">DECIMAL</option>
+            </select>
+          </td>
+        </tr>
+        <br />
+        <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-submission-type">Submission Type</label>
+          </td>
+          <td>
+            <select id="wd-group" name="wd-group">
+              <option value="">Select a group</option>
+              <option value="group1">Online</option>
+              <option value="group2">In Person</option>
+            </select>
+          </td>
+        </tr>
+        <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-online-entry-options">Online Entry</label>
+          </td>
+          <td>
+            <input type="checkbox" name="check-entry" id="wd-text-entry" />
+            <label htmlFor="wd-text-entry">Text Entry</label>
+            <br />
+
+            <input
+              type="checkbox"
+              name="check-entry"
+              id="wd-media-recordings"
+            />
+            <label htmlFor="wd-media-recordings">Website URL</label>
+            <br />
+
+            <input
+              type="checkbox"
+              name="check-entry"
+              id="wd-media-recordings"
+            />
+            <label htmlFor="wd-media-recordings">Media Recordings</label>
+            <br />
+
+            <input
+              type="checkbox"
+              name="check-entry"
+              id="wd-student-annotation"
+            />
+            <label htmlFor="wd-student-annotation">Student Annotation</label>
+            <br />
+            <input type="checkbox" name="check-genre" id="wd-file-uploads" />
+            <label htmlFor="wd-file-uploads">File Uploads</label>
+          </td>
+        </tr>
+        <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-assign-to">Assign to</label>
+          </td>
+          <td>
+            <input id="wd-assign-to" defaultValue={"Everyone"} />
+          </td>
+        </tr>
+        <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-due-date">Due</label>
+          </td>
+          <td>
+            <input type="date" id="wd-due-date" />
+          </td>
+        </tr>
+        <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-available-from">Available From</label>
+          </td>
+          <td>
+            <input type="date" id="wd-available-from" />
+          </td>
+        </tr>
+        <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-available-until">Until</label>
+          </td>
+          <td>
+            <input type="date" id="wd-available-until" />
+          </td>
+        </tr>
+        <tr>
+            <td align="right" valign="top">
+            <label htmlFor="cancel-btn">Cancel</label>
+          </td>
+          <td>
+            <button
+              id="cancel-btn"
+              type="button"
+
+            >
+              Cancel
+            </button>
+          </td>
+          <td align="right" valign="top">
+            <label htmlFor="submit-btn">Submit</label>
+          </td>
+          <td>
+            <button
+              id="submit-btn"
+              type="button"
+
+            >
+              Submit
+            </button>
+          </td>
+        </tr>
       </table>
     </div>
-);}
+  );
+}
