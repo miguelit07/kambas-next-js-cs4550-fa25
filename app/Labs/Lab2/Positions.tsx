6cf0d5bfef00e14d9f2f824190d9f24c8e4d13fe
miguelit07 <148008820+miguelit07@users.noreply.github.com>
2025-09-30 23:39:09 -0400

    up to 2.4

--- app/Labs/Lab2/Positions.tsx
+++ app/Labs/Lab2/Positions.tsx
@@ -43,6 +43,19 @@ export default function Positions() {
         <br />
         <br />
       </div>
+      <div id="wd-css-position-fixed">
+        <h2>Fixed position</h2>
+        Checkout the blue square that says &quot;Fixed position&quot; stuck all the way on
+        the right and half way down the page. It doesn&apos;t scroll with the rest of
+        the page. Its position is &quot;Fixed&quot;.
+        <div
+          className="wd-pos-fixed 
+    wd-dimension-square wd-bg-color-blue 
+    wd-fg-color-white"
+        >
+          Fixed position
+        </div>
+      </div>
     </div>
   );
 }
--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -7,10 +7,22 @@ import Margins from "./Margins";
 import Corners from "./Corners";
 import Dimensions from "./Dimensions";
 import Positions from "./Positions";
+import Zindex from "./Zindex";
+import Float from "./Float";
+import GridLayout from "./GridLayout";
+import Flex from "./Flex";
+import ReactIconsSampler from "./ReactIcons";
+import { Container } from "react-bootstrap";
+import BootstrapGrids from "./BootstrapGrids";
+import ScreenSizeLabel from "./ScreenSizeLabel";
+import BootstrapTables from "./BootstrapTables";
+import BootstrapLists from "./BootstrapLists";
+import BootstrapForms from "./BootstrapForms";
+import BootstrapNavigation from "./BootstrapNavigation";
 
 export default function Lab2() {
   return (
-    <div id="wd-lab2">
+    <Container>
       <h2>Lab 2 - Cascading Style Sheets</h2>
       <h3>Styling with the STYLE attribute</h3>
       <p>
@@ -75,6 +87,16 @@ export default function Lab2() {
       <Corners />
       <Dimensions />
       <Positions />
-    </div>
+      <Zindex />
+      <Float />
+      <GridLayout />
+      <Flex />
+      <ReactIconsSampler />
+      <BootstrapGrids />
+      <BootstrapTables />
+      <BootstrapLists />
+      <BootstrapForms />
+      <BootstrapNavigation />
+    </Container>
   );
 }
