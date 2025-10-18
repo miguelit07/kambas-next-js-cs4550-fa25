c9bf5359ceae76022fd3b6edb535e1b0769cd3b6
miguelit07 <148008820+miguelit07@users.noreply.github.com>
2025-10-17 20:20:08 -0400

    lint errors

--- app/Labs/Lab3/FindIndex.tsx
+++ app/Labs/Lab3/FindIndex.tsx
@@ -1,5 +1,5 @@
-let numberArray1 = [1, 2, 4, 5, 6];
-let stringArray1 = ['string1', 'string3'];
+const numberArray1 = [1, 2, 4, 5, 6];
+const stringArray1 = ['string1', 'string3'];
 
 export default function FindIndex() {
   const fourIndex = numberArray1.findIndex(a => a === 4);
