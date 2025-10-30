4fa2135222cd8bbd6f2a92071f36e06f7d52c5df
miguelit07 <148008820+miguelit07@users.noreply.github.com>
2025-10-30 18:42:48 -0400

    up to 4.2.27

---
+++ app/Labs/Lab4/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import ArrayStateVariable from "./ArrayStateVariable";
+import BooleanStateVariables from "./BooleanStateVariables";
+import ClickEvent from "./ClickEvent";
+import Counter from "./Counter";
+import DateStateVariables from "./DateStateVariables";
+import EventObject from "./EventObject";
+import ObjectStateVariable from "./ObjectStateVariable";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import StringStateVariables from "./StringStateVariables";
+
+export default function Lab4() {
+  function sayHello() {
+    alert("Hello");
+  }
+  return (
+    <div id="wd-lab4">
+      <h3>Lab 4</h3>
+      <ClickEvent />
+      <PassingDataOnEvent />
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject />
+      <Counter />
+      <BooleanStateVariables />
+      <StringStateVariables />
+      <DateStateVariables />
+      <ObjectStateVariable />
+      <ArrayStateVariable />
+    </div>
+  );
+}
