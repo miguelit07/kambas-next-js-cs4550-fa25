3d2c0742e796f4e15f2e1151c30cc0914aa11729
miguelit07 <148008820+miguelit07@users.noreply.github.com>
2025-10-30 20:37:38 -0400

    up to 4.4.2

--- app/(Kambaz)/Dashboard/page.tsx
+++ app/(Kambaz)/Dashboard/page.tsx
@@ -1,7 +1,8 @@
+"use client";
+import { useState } from "react";
 import Link from "next/link";
 import * as db from "../Database";
-
-import Image from "next/image";
+import { v4 as uuidv4 } from "uuid";
 import {
   Button,
   Card,
@@ -10,17 +11,86 @@ import {
   CardText,
   CardTitle,
   Col,
+  FormControl,
   Row,
 } from "react-bootstrap";
+
 export default function Dashboard() {
-  const courses = db.courses;
+  const [courses, setCourses] = useState<any[]>(db.courses);
+  const [course, setCourse] = useState<any>({
+    _id: "0",
+    name: "New Course",
+    number: "New Number",
+    startDate: "2023-09-10",
+    endDate: "2023-12-15",
+    image: "/images/reactjs.jpg",
+    description: "New Description",
+  });
+  const addNewCourse = () => {
+    const newCourse = { ...course, _id: uuidv4() };
+    setCourses([...courses, newCourse]);
+  };
+  const deleteCourse = (courseId: string) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
+
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
 
   return (
     <html>
       <body>
         <div id="wd-dashboard">
           <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-          <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
+          <h5>
+            New Course
+            <button
+              className="btn btn-primary float-end"
+              id="wd-add-new-course-click"
+              onClick={addNewCourse}
+            >
+              {" "}
+              Add{" "}
+            </button>
+            <button
+              className="btn btn-warning float-end me-2"
+              onClick={updateCourse}
+              id="wd-update-course-click"
+            >
+              Update{" "}
+            </button>
+          </h5>
+          <br />
+          <FormControl
+            value={course.name}
+            className="mb-2"
+            placeholder="Course Name"
+            onChange={(e) => setCourse({ ...course, name: e.target.value })}
+          />
+          <FormControl
+            value={course.description}
+            as="textarea"
+            rows={3}
+            className="mb-2"
+            placeholder="Course Description"
+            onChange={(e) =>
+              setCourse({ ...course, description: e.target.value })
+            }
+          />
+          <hr />
+          <h2 id="wd-dashboard-published">
+            Published Courses ({courses.length})
+          </h2>{" "}
+          <hr />
           <div id="wd-dashboard-courses">
             {/* Course 1 */}
             <Row xs={1} md={5} className="g-4">
@@ -49,6 +119,26 @@ export default function Dashboard() {
                           {course.description}{" "}
                         </CardText>
                         <Button variant="primary"> Go </Button>
+                        <button
+                          onClick={(event) => {
+                            event.preventDefault();
+                            deleteCourse(course._id);
+                          }}
+                          className="btn btn-danger float-end"
+                          id="wd-delete-course-click"
+                        >
+                          Delete
+                        </button>
+                        <button
+                          id="wd-edit-course-click"
+                          onClick={(event) => {
+                            event.preventDefault();
+                            setCourse(course);
+                          }}
+                          className="btn btn-warning me-2 float-end"
+                        >
+                          Edit
+                        </button>
                       </CardBody>
                     </Link>
                   </Card>
