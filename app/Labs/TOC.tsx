3a91fe1c3d4f4042d88fa5d6a23f4b7e076a4d0d
miguelit07 <148008820+miguelit07@users.noreply.github.com>
2025-11-14 19:35:19 -0500

    added toc links

--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -8,32 +8,56 @@ export default function TOC() {
   return (
     <Nav variant="pills">
       <NavItem>
-        <NavLink href="/Labs" as={Link}  className={`nav-link ${pathname.endsWith("Labs") ? "active" : ""}`}>
+        <NavLink
+          href="/Labs"
+          as={Link}
+          className={`nav-link ${pathname.endsWith("Labs") ? "active" : ""}`}
+        >
           Labs
         </NavLink>
       </NavItem>
       <NavItem>
-        <NavLink href="/Labs/Lab1" as={Link} className={`nav-link ${pathname.endsWith("Lab1") ? "active" : ""}`}>
+        <NavLink
+          href="/Labs/Lab1"
+          as={Link}
+          className={`nav-link ${pathname.endsWith("Lab1") ? "active" : ""}`}
+        >
           Lab 1
         </NavLink>
       </NavItem>
       <NavItem>
-        <NavLink href="/Labs/Lab2" as={Link} className={`nav-link ${pathname.endsWith("Lab2") ? "active" : ""}`}>
+        <NavLink
+          href="/Labs/Lab2"
+          as={Link}
+          className={`nav-link ${pathname.endsWith("Lab2") ? "active" : ""}`}
+        >
           Lab 2
         </NavLink>
       </NavItem>
       <NavItem>
-        <NavLink href="/Labs/Lab3" as={Link} className={`nav-link ${pathname.endsWith("Lab3") ? "active" : ""}`}>
+        <NavLink
+          href="/Labs/Lab3"
+          as={Link}
+          className={`nav-link ${pathname.endsWith("Lab3") ? "active" : ""}`}
+        >
           Lab 3
         </NavLink>
       </NavItem>
       <NavItem>
-        <NavLink href="/Labs/Lab4" as={Link} className={`nav-link ${pathname.endsWith("Lab4") ? "active" : ""}`}>
+        <NavLink
+          href="/Labs/Lab4"
+          as={Link}
+          className={`nav-link ${pathname.endsWith("Lab4") ? "active" : ""}`}
+        >
           Lab 4
         </NavLink>
       </NavItem>
       <NavItem>
-        <NavLink href="/Labs/Lab5" as={Link} className={`nav-link ${pathname.endsWith("Lab5") ? "active" : ""}`}>
+        <NavLink
+          href="/Labs/Lab5"
+          as={Link}
+          className={`nav-link ${pathname.endsWith("Lab5") ? "active" : ""}`}
+        >
           Lab 5
         </NavLink>
       </NavItem>
@@ -45,6 +69,16 @@ export default function TOC() {
       <NavItem>
         <NavLink href="https://github.com/miguelit07/kambas-next-js-cs4550-fa25">
           My GitHub
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="https://github.com/miguelit07/kambaz-node-server-app">
+          New Server Repository Link
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="https://kambaz-node-server-app-s5sn.onrender.com">
+          Server Root Link
         </NavLink>
       </NavItem>
     </Nav>
